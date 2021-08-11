62432a5acd4ad3915ce445112246766a57c6844f
Yousef-Nour <yousefnour809@gmail.com>
2021-08-11 07:24:45 -0700

    Header and Slider

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-<<<<<<< HEAD
-import Header from './components/login/Header'
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/login/Header";
+import Registeration from "./views/Registeration";
 
 function App() {
   return (
     <div className="App">
-      
-      <Header pageTitle="Axsos"/>
-=======
-import Registeration from './views/Registeration';
-function App() {
-  return (
-    <div className="App">
-      <Registeration/>
->>>>>>> 89648d52c3840d98e18e4c278922998bb82be9b5
+      {/* <Registeration /> */}
+      <Header pageTitle="Axsos" />
     </div>
   );
 }
