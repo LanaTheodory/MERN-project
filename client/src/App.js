8c8eb75ecd3fef9aa0fff6604aa8775af8a836fd
Yousef-Nour <yousefnour809@gmail.com>
2021-08-12 05:35:47 -0700

    merge the components

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,18 @@ import "./App.css";
 import Header from "./components/login/Header";
 import Registeration from "./views/Registeration";
 // import PostComment from './components/PostComponent/Post';
-import { Router } from '@reach/router';
-import MainPage from './views/MainPage';
-import AboutAXSOS from "./components/aboutAxsos/AboutAXSOS";
+import { Router } from "@reach/router";
+import MainPage from "./views/MainPage";
+
 // import { Slider } from "@material-ui/core";
-import ImagesSlider from "./components/aboutAxsos/ImagesSlider";
+
 function App() {
   return (
-    <div className="App">
-     {/* <Router>
-      <Registeration path="/register" />
-      <Header path="/" pageTitle="Axsos" />
-       <MainPage path="/"></MainPage>
-     </Router> */}
-     {/* <ImagesSlider/> */}
-     <AboutAXSOS/>
-     {/* <Header/> */}
-      {/* <Registeration /> */}
-     <Router>
-      <Registeration path="/register" />
-      <Header path="/" pageTitle="Axsos" />
-       <MainPage path="/"></MainPage>
-     </Router>
-     <Header/>
-   
-    </div>
+    //   {/* <Header/> */}
+      <Router>
+       <Registeration path="/register" />
+        <MainPage path="/" />
+      </Router>
   );
 }
 
