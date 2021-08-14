ee8bb4082b7032848ecf6a9bf18e7d16b26455c4
LanaTheodory <81506605+LanaTheodory@users.noreply.github.com>
2021-08-14 14:21:08 +0300

    updated loading state

--- client/src/views/MainPage.js
+++ client/src/views/MainPage.js
@@ -82,7 +82,7 @@ const MainPage = (props) => {
               );
             })
           ) : (
-            <p>loading......</p>
+            <p>loading struggles...</p>
           )}
         </div>
       </div>
